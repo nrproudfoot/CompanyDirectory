e26e67dde116a0ccb51a643067a539bc6cfbb2ae
nrproudfoot <72758838+nrproudfoot@users.noreply.github.com>
2021-03-17 10:39:33 +0000

    Update config.php

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,9 +1,9 @@
 <?php
-  $cd_host = 'db5001651824.hosting-data.io';
-  $cd_port = 3306;
+  $cd_host = '';
+  $cd_port = ;
   $cd_socket = "";
-  $cd_user = 'dbu1678127'; // user name
-  $cd_password = 'N3ck3rch13f539!'; // password
-  $cd_dbname = "dbs1370447"; // database name
+  $cd_user = ''; // user name
+  $cd_password = ''; // password
+  $cd_dbname = ""; // database name
 
 ?>
